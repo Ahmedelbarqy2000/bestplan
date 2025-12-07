0c1b0e1b569bd94e279c5e0bdccbfc755ed16204
Ahmedelbarqy2000 <portegas.ahmed@gmail.com>
2025-12-07 07:23:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // تأكد أن ملف الـ CSS موجود حتى لو فارغ إذا استخدمت CDN
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
